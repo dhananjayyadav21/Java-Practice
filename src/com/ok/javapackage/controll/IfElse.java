e9ea8ffe53a2a661784366605716c922629278a7
dhananjay-yadav-it <dhananjayyadav1807@gmail.com>
2025-08-15 14:17:54 +0530

    Learn string and looping

--- src/com/ok/javapackage/controll/IfElse.java
+++ src/com/ok/javapackage/controll/IfElse.java
@@ -5,30 +5,64 @@ import java.util.Scanner;
 public class IfElse {
     public static  void  main(String [] args)
     {
-        System.out.println("Enter You Day No :");
         Scanner sc = new Scanner(System.in);
-        int day = sc.nextInt();
-        String dayName ;
 
-        if(day==1)
-        {
-            dayName="Monday";
-        } else if (day==2) {
-            dayName="Tuesday";
-        } else if (day==3) {
-            dayName="Wednesday";
-        }else if (day==4) {
-            dayName="Thursday";
-        } else if (day==5) {
-            dayName="Friday";
-        } else if (day==6) {
-            dayName="Saturday";
-        }else if (day==7) {
-            dayName="Sunday";
-        }else{
-            dayName="Invalid Day, please try again";
-        }
+//      1) find days base on number 1 to 7 ====================================
+//        System.out.println("Enter You Day No :");
+//        int day = sc.nextInt();
+//        String dayName ;
+//
+//        if(day==1)
+//        {
+//            dayName="Monday";
+//        } else if (day==2) {
+//            dayName="Tuesday";
+//        } else if (day==3) {
+//            dayName="Wednesday";
+//        }else if (day==4) {
+//            dayName="Thursday";
+//        } else if (day==5) {
+//            dayName="Fri day";
+//        } else if (day==6) {
+//            dayName="Saturday";
+//        }else if (day==7) {
+//            dayName="Sunday";
+//        }else{
+//            dayName="Invalid Day, please try again";
+//        }
+//        System.out.print(dayName);
 
-        System.out.println("Day is a : "+dayName);
+        // 2) PAss or fail ====================================
+//        System.out.println("Enter here your marks : ");
+//        float subj1 = sc.nextInt();
+//        float subj2 = sc.nextInt();
+//        float subj3 = sc.nextInt();
+//
+//        float totalPercent = ((subj1+subj2+subj3)/300 ) *100 ;
+//
+//        if((totalPercent >= 40 )|| (subj1 >= 33 && subj2 >= 33 && subj3 >= 33)){
+//            System.out.println("You are pass");
+//        }else{
+//            System.out.println("You are not pass");
+//        }
+
+//      3) find tax on sal ====================================
+//        System.out.print("Enter Your Sal here : ");
+//        float sall = sc.nextInt();
+//        float tax = 0;
+//
+//        if(sall <= 250000){
+//            System.out.println("You are the below the range of income tax");
+//        }else{
+//            if(sall > 250000 || sall < 500000){
+//                tax = (sall*5)/100;
+//            } else if (sall > 500000 || sall < 1000000) {
+//                tax = (sall*20)/100;
+//            }else if(sall > 1000000) {
+//                tax = (sall*30)/100;
+//            }
+//            System.out.print("The tax on your " + sall + " is : " + tax);
+//        }
+
     }
 }
---
+++ src/com/ok/javapackage/controll/Looping.java
@@ -0,0 +1,47 @@
+package com.ok.javapackage.controll;
+
+import java.util.Scanner;
+
+public class Looping {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+//      1) Write a program to print pattern
+//         for(int i = 4; i > 0; i--){
+//             System.out.println();
+//             for(int j = 1; j <= i ; j++){
+//                    System.out.print("* ");
+//             }
+//         }
+
+//       2) Write a program to print multiplication of a given number
+//         System.out.println();
+//         System.out.print("Enter Here Number : ");
+//         int tableNumber = sc.nextInt();
+//
+//         for(int i = 1; i <= 10; i++){
+//             System.out.println(tableNumber * i);
+//         }
+
+//       3) Write a program to print multiplication of a given number in reverse
+//         System.out.println();
+//         System.out.print("Enter Here Number : ");
+//         int tableReverseNumber = sc.nextInt();
+//
+//         for(int i = 10; i > 0; i--){
+//             System.out.println(tableReverseNumber * i);
+//         }
+
+
+//       4) Write a program to print factorial of a given number in reverse
+//         System.out.println();
+//         System.out.print("Enter Here Number : ");
+//         int factNum = sc.nextInt();
+//         int fact = 1;
+//
+//         for(int i = factNum; i > 0; i--){
+//             fact *=i;
+//         }
+//         System.out.println("The fact is " + fact);
+    }
+}

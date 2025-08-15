462f7d6fb7d41e6ddd7839fc3d0acd52984a5e88
dhananjay-yadav-it <dhananjayyadav1807@gmail.com>
2025-08-15 10:16:56 +0530

    initial commit

---
+++ src/com/ok/javapackage/datatype/Main.java
@@ -0,0 +1,19 @@
+package com.ok.javapackage.datatype;
+
+public class Main {
+    public static void main(String[] args) {
+        byte a= 12;
+        short b= 125;
+        int c= 1452;
+        long d= 1452L;
+
+        float f= 1452.25f;
+        double e= 14556.2;
+
+
+        boolean isAge = true;
+
+        System.out.println("isAge:"+isAge);
+        System.out.println(a + b + c + d + e + f  );
+    }
+}
